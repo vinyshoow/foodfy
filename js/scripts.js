bd7956c1799abd837c6bf9f8d719f344bfb3b0ec
vinyshoow <viniciuslopes.qz@hotmail.com>
2020-02-25 20:36:09 -0300

    commit inicial

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+const modaloverlay = document.querySelector('.modal-overlay');
+
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        
+        const imageid = card.getAttribute('id');
+        const title = card.querySelector('.card_content').innerHTML;
+        const author = card.querySelector('.card_info').innerHTML;
+                
+        modaloverlay.classList.add('active');
+        modaloverlay.querySelector('img').src= `img/${imageid}.png`;
+        modaloverlay.querySelector('.modal-title').innerHTML = `${title}`;
+        modaloverlay.querySelector('.modal-author').innerHTML = `${author}`;
+        
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+    modaloverlay.classList.remove('active')
+
+})
