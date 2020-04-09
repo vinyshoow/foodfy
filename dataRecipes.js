5fa3d966b54eeb6748703044f7eb6dfeebe459af
vinyshoow <viniciuslopes.qz@hotmail.com>
2020-04-09 10:39:15 -0300

    Refatorando Foodfy - Parte 2

---
+++ dataRecipes.js
@@ -0,0 +1,74 @@
+module.exports = [
+  {
+    id: "burger",
+    img_url: "/burger.png",
+    title: "Triplo Bacon Burguer",
+    description: "por Jorge Relato"
+  },
+  {
+    id: "pizza",
+    img_url: "/pizza.png",
+    title: "Pizza 4 estações",
+    description: "por JFabiana Melo"
+  },
+  {
+    id: "espaguete",
+    img_url: "/espaguete.png",
+    title: "Espaguete ao alho",
+    description: "por Júlia Kinoto"
+  },
+  {
+    id: "lasanha",
+    img_url: "/lasanha.png",
+    title: "Lasanha mac n' cheese",
+    description: "por Juliano Vieira"
+  },
+  {
+    id: "doce",
+    img_url: "/doce.png",
+    title: "Docinhos pão-do-céu",
+    description: "por Ricardo Golvea"
+  },
+  {
+    id: "asinhas",
+    img_url: "/asinhas.png",
+    title: "Asinhas de frango ao barbecue",
+    description: "por Vania Steroski"
+  },
+  {
+    id: "burger",
+    img_url: "/burger.png",
+    title: "Triplo Bacon Burguer",
+    description: "por Jorge Relato"
+  },
+  {
+    id: "pizza",
+    img_url: "/pizza.png",
+    title: "Pizza 4 estações",
+    description: "por JFabiana Melo"
+  },
+  {
+    id: "espaguete",
+    img_url: "/espaguete.png",
+    title: "Espaguete ao alho",
+    description: "por Júlia Kinoto"
+  },
+  {
+    id: "lasanha",
+    img_url: "/lasanha.png",
+    title: "Lasanha mac n' cheese",
+    description: "por Juliano Vieira"
+  },
+  {
+    id: "doce",
+    img_url: "/doce.png",
+    title: "Docinhos pão-do-céu",
+    description: "por Ricardo Golvea"
+  },
+  {
+    id: "asinhas",
+    img_url: "/asinhas.png",
+    title: "Asinhas de frango ao barbecue",
+    description: "por Vania Steroski"
+  }
+]
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const receitas = require('./data');
+const dataRecipes = require('./dataRecipes');
 
 const app = express();
 
@@ -16,7 +18,7 @@ nunjucks.configure("views", {
 
 
 app.get('/', function(req, res){
-  return res.render("home");
+  return res.render("home", {items: receitas});
 });
 
 app.get('/about', function(req, res){
@@ -24,7 +26,7 @@ app.get('/about', function(req, res){
 })
 
 app.get('/recipes', function(req, res){
-  return res.render("recipes");
+  return res.render("recipes", {items: dataRecipes});
 })
 
 app.use(function(req,res){
