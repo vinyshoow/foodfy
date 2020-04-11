57ec7e9827d81f638c142ed681d57347b09ef78e
vinyshoow <viniciuslopes.qz@hotmail.com>
2020-04-11 16:39:55 -0300

    Refatorando FoodFy da forma Correta

--- public/scripts.js
+++ public/scripts.js
@@ -1,23 +1,83 @@
-const modaloverlay = document.querySelector('.modal-overlay');
+/* const modaloverlay = document.querySelector('.modal-overlay'); */
 
 const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
     card.addEventListener("click", function(){
         
-        const imageid = card.getAttribute('id');
-        const title = card.querySelector('.card_content').innerHTML;
+        const recipeId = card.getAttribute('id');
+        window.location.href = `/recipes/0`;
+
+        /* const title = card.querySelector('.card_content').innerHTML;
         const author = card.querySelector('.card_info').innerHTML;
                 
         modaloverlay.classList.add('active');
         modaloverlay.querySelector('img').src= `/${imageid}.png`;
         modaloverlay.querySelector('.modal-title').innerHTML = `${title}`;
-        modaloverlay.querySelector('.modal-author').innerHTML = `${author}`;
+        modaloverlay.querySelector('.modal-author').innerHTML = `${author}`; */
         
     })
 }
 
-document.querySelector(".close-modal").addEventListener("click", function() {
+/* document.querySelector(".close-modal").addEventListener("click", function() {
     modaloverlay.classList.remove('active')
 
-})
+}) */
+
+
+function showHideIng(){
+    let display = document.querySelector('#showHideIng').style.display;
+    let showHide = document.querySelector('.showHideIng');
+
+    document
+      .querySelector("#showHideIng")
+      .classList
+      .toggle("hide");
+    
+    if(display == "none") {
+        document.querySelector('#showHideIng').style.display = 'block';
+        showHide.innerHTML = "ESCONDER";
+    }
+    else {
+        document.querySelector('#showHideIng').style.display = 'none';
+        showHide.innerHTML = "MOSTRAR";
+    }
+}
+
+function showHidePrep(){
+    let display = document.querySelector('#showHidePrep').style.display;
+    let showHide = document.querySelector('.showHidePrep');
+
+    document
+      .querySelector("#showHidePrep")
+      .classList
+      .toggle("hide");
+    
+    if(display == "none") {
+        document.querySelector('#showHidePrep').style.display = 'block';
+        showHide.innerHTML = "ESCONDER";
+    }
+    else {
+        document.querySelector('#showHidePrep').style.display = 'none';
+        showHide.innerHTML = "MOSTRAR";
+    }
+}
+
+function showHideInfo(){
+    let display = document.querySelector('#showHideInfo').style.display;
+    let showHide = document.querySelector('.showHideInfo');
+
+    document
+      .querySelector("#showHideInfo")
+      .classList
+      .toggle("hide");
+    
+    if(display == "none") {
+        document.querySelector('#showHideInfo').style.display = 'block';
+        showHide.innerHTML = "ESCONDER";
+    }
+    else {
+        document.querySelector('#showHideInfo').style.display = 'none';
+        showHide.innerHTML = "MOSTRAR";
+    }
+}
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const receitas = require('./data');
-const dataRecipes = require('./dataRecipes');
 
 const app = express();
 
@@ -14,9 +13,6 @@ nunjucks.configure("views", {
   noCache: true
 })
 
-
-
-
 app.get('/', function(req, res){
   return res.render("home", {items: receitas});
 });
@@ -25,8 +21,23 @@ app.get('/about', function(req, res){
   return res.render("about");
 })
 
-app.get('/recipes', function(req, res){
-  return res.render("recipes", {items: dataRecipes});
+/* app.get('/recipes', function(req, res){
+  const id = req.query.id;
+  const recipe = receitas.find(function(recipe){
+    return recipe.id == id;
+    
+  })
+
+  return res.render("recipes", {item: receitas});
+})
+ */
+app.get("/recipes/:index", function (req, res) {
+  const recipe = []; // Array de receitas carregadas do data.js
+  const recipeIndex = req.params.index;
+
+  console.log(receitas[recipeIndex]);
+
+  return res.render("recipes", {item: receitas[recipeIndex]});
 })
 
 app.use(function(req,res){
